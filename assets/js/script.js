5b04e7bb982379041491cb3890a0e4d0d979d84d
Anderson Souza <agsvip@yahoo.com.br>
2024-08-27 22:57:32 -0300

    Quinto commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,70 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+// document.getElementById('contactForm').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Impede o envio padrão do formulário
     
-    // Limpar mensagens de erro
-    document.getElementById('nameError').style.display = 'none';
-    document.getElementById('emailError').style.display = 'none';
-    document.getElementById('messageError').style.display = 'none';
+//     // Limpar mensagens de erro
+//     document.getElementById('nameError').style.display = 'none';
+//     document.getElementById('emailError').style.display = 'none';
+//     document.getElementById('messageError').style.display = 'none';
 
-    // Obter valores dos campos
-    var name = document.getElementById('name').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var message = document.getElementById('message').value.trim();
+//     // Obter valores dos campos
+//     var name = document.getElementById('name').value.trim();
+//     var email = document.getElementById('email').value.trim();
+//     var message = document.getElementById('message').value.trim();
 
-    var isValid = true;
+//     var isValid = true;
 
-    // Validar nome
-    if (name === '') {
-        document.getElementById('nameError').innerText = 'Por favor, preencha seu nome.';
-        document.getElementById('nameError').style.display = 'block';
-        isValid = false;
-    }
+//     // Validar nome
+//     if (name === '') {
+//         document.getElementById('nameError').innerText = 'Por favor, preencha seu nome.';
+//         document.getElementById('nameError').style.display = 'block';
+//         isValid = false;
+//     }
 
-    // Validar e-mail
-    if (email === '') {
-        document.getElementById('emailError').innerText = 'Por favor, preencha seu e-mail.';
-        document.getElementById('emailError').style.display = 'block';
-        isValid = false;
-    } else if (!validateEmail(email)) {
-        document.getElementById('emailError').innerText = 'Por favor, insira um e-mail válido.';
-        document.getElementById('emailError').style.display = 'block';
-        isValid = false;
-    }
+//     // Validar e-mail
+//     if (email === '') {
+//         document.getElementById('emailError').innerText = 'Por favor, preencha seu e-mail.';
+//         document.getElementById('emailError').style.display = 'block';
+//         isValid = false;
+//     } else if (!validateEmail(email)) {
+//         document.getElementById('emailError').innerText = 'Por favor, insira um e-mail válido.';
+//         document.getElementById('emailError').style.display = 'block';
+//         isValid = false;
+//     }
 
-    // Validar mensagem
-    if (message === '') {
-        document.getElementById('messageError').innerText = 'Por favor, escreva sua mensagem.';
-        document.getElementById('messageError').style.display = 'block';
-        isValid = false;
-    }
+//     // Validar mensagem
+//     if (message === '') {
+//         document.getElementById('messageError').innerText = 'Por favor, escreva sua mensagem.';
+//         document.getElementById('messageError').style.display = 'block';
+//         isValid = false;
+//     }
 
-    // Se todos os campos forem válidos
-    if (isValid) {
-        alert('Formulário enviado com sucesso!');
-        // Aqui você pode adicionar o código para enviar o formulário, se necessário.
+//     // Se todos os campos forem válidos
+//     if (isValid) {
+//         alert('Formulário enviado com sucesso!');
+//         // Aqui você pode adicionar o código para enviar o formulário, se necessário.
+//     }
+// });
+
+// function validateEmail(email) {
+//     var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return re.test(email);
+// }
+
+
+// SCROLL MENU
+
+window.addEventListener('scroll', function(){
+    let header = document.querySelector('header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
+
+// BOTAO RETORNO AO TOPO
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY >= 400){
+        goTopBtn.classList.add("active");
+    }else{
+        goTopBtn.classList.remove("active");
     }
 });
-
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
